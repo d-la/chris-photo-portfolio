410608dcb6d98e6773d7f312ec952f79b19a2275
Daniel La <la.danieln@gmail.com>
2019-02-16 21:31:03 -0500

    Only add event listener if the element is found (theres no element on login page)

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -4,6 +4,8 @@ const navigationMenu = document.querySelector('.nav__list');
 
 const navigationToggle = document.querySelector('.nav__toggle');
 
-navigationToggle.addEventListener('click', () => {
-    navigationMenu.classList.toggle('nav__list--active');
-});
+if (navigationToggle != undefined){
+    navigationToggle.addEventListener('click', () => {
+        navigationMenu.classList.toggle('nav__list--active');
+    });
+}
