54c569fe617b4cf655a63de10b21049291af9f81
Daniel La <la.danieln@gmail.com>
2019-02-18 20:13:12 -0500

    Possible bug fix for mysql connections and also added logic to fetch or insert data for specific post routes

--- routes/back-end.js
+++ routes/back-end.js
@@ -4,13 +4,21 @@ let router = express.Router();
 let mysql = require('mysql'),
     bcrypt = require('bcrypt');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host:     'localhost',
+//     user:     'root',
+//     password: 'root',
+//     database: 'photo_blog',
+//     port: 8889
+// });
+
+const connectionInfo = {
     host:     'localhost',
     user:     'root',
     password: 'root',
     database: 'photo_blog',
     port: 8889
-});
+};
 
 // Show the login view
 router.get('/login', (req, res) => {
@@ -29,6 +37,7 @@ router.post('/login', (req, res) => {
     };
 
     // Logic to get password_hash from database
+    const connection = mysql.createConnection(connectionInfo);
     connection.connect();
     connection.query(`SELECT id, first_name, last_name, email, password_hash FROM users WHERE email = '${userData.email}';`, (error, results, fields) => {
         if (error){
@@ -62,11 +71,91 @@ router.get('/admin/dashboard', (req, res) => {
 });
 
 router.get('/admin/albums', (req, res) => {
-    res.render('admin/albums');
+
+    const connection = mysql.createConnection(connectionInfo);
+    connection.connect();
+
+    const sqlQuery = `SELECT id, title, description, location, date_added FROM albums;`;
+    
+    let albums;
+    connection.query(sqlQuery, (error, results, fields) => {
+        if (error){
+            console.log(error);
+            throw error;
+        } else {
+            albums = results;
+
+            res.render('admin/albums', {albums: albums});
+        }
+    });
+    connection.end();
+});
+
+router.post('/admin/albums', (req, res) => {
+    // Collect the data from the form
+    const album ={
+        title: req.body.album.title,
+        description: req.body.album.description,
+        location: req.body.album.location
+    };
+
+    // Get the current date
+    const currentTime = new Date();
+
+    // Extract the month and correct the value since months from the Date object start at 0
+    let currentMonth = currentTime.getMonth() + 1;
+    if (currentMonth < 10){
+        currentMonth = '0' + currentMonth;
+    }
+    // Final value of the current date in MySQL Format
+    const currentDate = `${currentTime.getFullYear()}-${currentMonth}-${currentTime.getDate()}`;
+
+    const connection = mysql.createConnection(connectionInfo);
+    connection.connect();
+
+    // const sqlQuery = "INSERT INTO albums (title, description, location, date_added) VALUES (" + connection.escape(album.title)  + ", " + connection.escape(album.description) + ". " + connection.escape(album.location) + ", " + currentDate + ")";
+    const sqlQuery = `INSERT INTO albums (title, description, location, date_added) VALUES (${connection.escape(album.title)}, ${connection.escape(album.description)}, ${connection.escape(album.location)}, '${currentDate}');`;
+    
+    connection.query(sqlQuery, (error, results, fields) => {
+        if (error){
+            throw error;
+        } else {
+            console.log(results);
+            // console.log(fields);
+
+            // const affectedRows = results[0].affectedRows;
+
+            // if (affectedRows === 1){
+            //     // Some session object to denote success
+            // } else {
+            //     // Some session object to denote error
+            // }
+
+            // res.redirect('/admin/albums');
+        }
+    });
+    connection.end();
 });
 
 router.get('/admin/images', (req, res) => {
-    res.render('admin/images');
+    const connection = mysql.createConnection(connectionInfo);
+    connection.connect();
+
+    const sqlQuery = `SELECT id, title FROM albums;`;
+
+    connection.query(sqlQuery, (error, results, fields) => {
+        if (error){
+            throw error;
+        } else {
+            const allAlbums = results;
+
+            res.render('admin/images', {
+                allAlbums: allAlbums
+            });
+        }
+    });
+
+    // res.render('admin/images');
 });
 
 module.exports = router;
