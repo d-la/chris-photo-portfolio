6b11412addfcd7e446531b0d6be1aa9610603b30
Daniel La <la.danieln@gmail.com>
2019-02-18 21:09:03 -0500

    Wrap the mysql package into a class and return promises for multiple queries and other functionality

---
+++ models/MysqlDB.js
@@ -0,0 +1,39 @@
+let mysql = require('mysql');
+
+class MysqlDB {
+    // constructor( config ){
+    //     this.connection = mysql.createConnection(config);
+    // }
+
+    initializeConnection( config ){
+        this.connection = mysql.createConnection( config );
+    }
+
+    // Return a promise when executing a query
+    executeQuery( sqlQuery, args ){
+        return new Promise ( (resolve, reject) => {
+            this.connection.query( sqlQuery, args, (error, results) => {
+                if ( error ){
+                    return reject( error );
+                } else {
+                    resolve( results );
+                }
+            })
+        });
+    }
+
+    // Return a promise when closing the connection
+    closeConnection(){
+        return new Promise( (resolve, reject) => {
+            this.connection.end( error => {
+                if ( error ){
+                    return reject( error );
+                } else {
+                    resolve();
+                }
+            })
+        });
+    }
+}
+
+module.exports = new MysqlDB();
