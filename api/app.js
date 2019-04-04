881a86888926b6dd4406bfe47839ddc80fcae82a
Daniel La <la.danieln@gmail.com>
2019-04-03 23:51:55 -0400

    Updates to main app.js file for API refactoring

--- api/app.js
+++ api/app.js
@@ -1,88 +1,28 @@
 require('dotenv').config();
 
 let express = require('express'),
-    bodyParser = require('body-parser')
-    path = require('path'),
-    expressSanitizer = require('express-sanitizer'),
-    session = require('express-session'),
-    mysql = require('mysql')
-    bcrypt = require('bcrypt')
-    fileUpload = require('express-fileupload'),
-    fs = require('fs'),
-    multer = require('multer'),
-    methodOverride = require('method-override');
+    bodyParser = require('body-parser'),
+    mysql = require('mysql'),
+    categoryRoutes = require('./routes/category');
 
-let frontEndRoutes = require('./routes/front-end'),
-    backEndRoutes = require('./routes/back-end');
+const errorHandler = require('./handlers/error');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressSanitizer());
+app.use(bodyParser.json());
 
-// Use method-override to use PUT/DELETE requests
-app.use(methodOverride('_method'));
+app.use('/api/category', categoryRoutes);
 
-// Application routes
-app.use(frontEndRoutes);
-app.use(backEndRoutes);
-
-// Handle the file upload in this file since it wont work inside back-end.js?
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
-    }
+app.use( (req, res, next) => {
+    let error = new Error('Not Found');
+    error.status = 404;
+    next(error);
 });
 
-const upload = multer({storage: storage});
-app.post('/admin/images', upload.single('albumImage'), (req, res) => {
-
-    // Todays date
-    todaysDate = require('./models/todays-date');
-
-    let uploadedFileName = req.file.filename;
-    let fileDestination = `/img/${uploadedFileName}`;
-
-    const imageData = {
-        title: req.body.image.title,
-        description: req.body.image.description,
-        albumId: req.body.albumId
-    };
-
-    const insertImageDataQuery = `INSERT INTO images (src, title, description, album_id, date_added) VALUES ('${fileDestination}', ${mysql.escape(imageData.title)}, ${mysql.escape(imageData.description)}, ${imageData.albumId}, '${todaysDate}');`;
-    
-    mysqlDB = require('./models/MysqlDB.js');
-
-    mysqlDB.initializeConnection({
-        host:     process.env.DB_HOST,
-        user:     process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
-    });
-
-    mysqlDB.executeQuery(insertImageDataQuery).then( (result) => {
-
-        if (result === true){
-            // Success logic
-        } else {
-            // Error logic
-        }
-        mysqlDB.closeConnection();
-
-        res.redirect('/admin/images');
-    }).catch( (error) => {
-        console.log(error);
-    });
-});
+app.use(errorHandler);
 
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Server is running!');
+    console.log(`Server is running on port ${port}!`);
 });
