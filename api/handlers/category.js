1bac755d12e9f5656d4b8cc7a56e303ed77c822e
Daniel La <la.danieln@gmail.com>
2019-04-04 17:35:49 -0400

    Put route and handler added

--- api/handlers/category.js
+++ api/handlers/category.js
@@ -103,7 +103,7 @@ exports.getCategoryData = async function(req, res, next){
         // Create query to select category data
         // const selectCategoryData = `SELECT c.category_id, c.category_title, c.category_desc, s.subcategory_id, s.subcategory_title, s.subcategory_desc, p.photo_id, p.photo_title, p.photo_desc, p.photo_src FROM category c INNER JOIN subcategory s ON c.category_id = s.subcategory_id RIGHT JOIN photo p ON p.subcategory_id = s.subcategory_id WHERE c.category_id = ${req.body.id};`
         const selectCategoryData = `SELECT category_id, category_title, category_desc FROM category WHERE category_id = ${req.params.id};`;
-
+        
         let response = await mysqlDB.executeQuery(selectCategoryData);
         
         res.status(200).json(response);
@@ -111,6 +111,36 @@ exports.getCategoryData = async function(req, res, next){
         return next({
             status: 400,
             message: 'Unable to retrieve category data'
+        });
+    }
+}
+
+exports.updateCategory = async function(req, res, next){
+    try {
+
+        mysqlDB.initializeConnection(connectionInfo);
+        const updateCategoryData = `UPDATE category SET category_title = ${mysql.escape(req.body.title)}, category_desc = ${mysql.escape(req.body.desc)} WHERE category_id = ${req.params.id};`;
+
+        let response = await mysqlDB.executeQuery(updateCategoryData);
+
+        if (response.affectedRows === 1){
+            res.status(200).json({
+                status: true,
+                category_id: req.params.id,
+                category_title: req.body.title,
+                category_desc: req.body.desc
+            });
+        } else {
+            return next({
+                status: 400,
+                message: 'Unable to update category data'
+            });
+        }
+
+    } catch (error) {
+        return next({
+            status: 400,
+            message: 'Unable to update category data'
         });
     }
 }
--- api/routes/category.js
+++ api/routes/category.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { allCategoryData, insertCategory, deleteCategory, getCategoryData } = require('../handlers/category');
+const { allCategoryData, insertCategory, deleteCategory, getCategoryData, updateCategory } = require('../handlers/category');
 
 // Get all category data
 router.get('/', allCategoryData);
 router.post('/', insertCategory);
 router.delete('/', deleteCategory);
 router.get('/:id', getCategoryData);
+router.put('/:id', updateCategory);
 
 module.exports = router;
