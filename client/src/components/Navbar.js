73097655e8cc9aebb8816586546126e1e3ae4276
Daniel La <la.danieln@gmail.com>
2019-04-09 12:49:00 -0400

    Rename component to Navbar and add new method that toggles the navbar on small screens

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            navbarClass: 'nav__list'
+        }
+
+        this.handleNavbarToggle = this.handleNavbarToggle.bind(this);
+    }
+
+    handleNavbarToggle(){
+        if (this.state.navbarClass === 'nav__list'){
+            this.setState({ navbarClass: 'nav__list nav__list--active' });
+        } else {
+            this.setState({ navbarClass: 'nav__list' });
+        }
+    }
+
+    render(){
+
+        const { categories } = this.props;
+        const { navbarClass } = this.state;
+
+        let categoryListItems = categories.map( category => (
+            <li className="nav__item" key={category.category_id}>
+                <Link to="/contact" className="nav__links">
+                    { category.category_title }
+                </Link>
+            </li>
+        ));
+
+        return(
+            <nav className="nav">
+                <span className="nav__toggle " onClick={this.handleNavbarToggle}>
+                    &#9776;
+                </span>
+                <Link to="/" className="nav__logo flex">
+                    <span className="nav__logo-name">
+                        CD
+                    </span>
+                </Link>
+
+                <ul className={navbarClass}>
+                    { categoryListItems }
+                    <li className="nav__item">
+                        <Link to="/contact" className="nav__links">Contact</Link>
+                    </li>
+                </ul>
+
+            </nav>
+        )
+    }
+}
+
+export default Header;
