f11d77dffe45e76eb8de5d0af5b2eb237d7837cb
Daniel La <la.danieln@gmail.com>
2019-04-08 23:44:23 -0400

    Fetch categories from API, pass them as components to Header, put categories on navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,28 @@ import './css/styles.min.css';
 import Header from './components/header';
 
 class App extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			categories: []
+		}
+	}
+
+	componentWillMount(){
+		fetch('http://localhost:3000/api/category')
+			.then( data => data.json() )
+			.then( data => this.setState({categories: data}) );
+	}
+
 	render() {
+
+		const { categories } = this.state;
+
 		return (
 			<div>
-				<Header />
+				<Header categories={categories} />
 				<div className="welcome">
 					<div className="welcome__message">
 						<h1>Welcome to Christopher Dinh's Photography Portfolio</h1>
