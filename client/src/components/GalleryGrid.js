01ac6b50677085956e4540976bf5a2e37ab4b955
Daniel La <la.danieln@gmail.com>
2019-04-10 01:13:47 -0400

    Add method formatDate to convert MySQL date to YYYY-MM-DD and display on front end

--- client/src/components/GalleryGrid.js
+++ client/src/components/GalleryGrid.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 
 class GalleryGrid extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.formatDate = this.formatDate.bind(this);
+    }
+
+    /**
+     * Convert a MySQL formatted date into YYYY-MM-DD
+     * 
+     * @param {date} date date to be converted
+     * 
+     * @return {string}
+     */
+    formatDate(date){
+        let newDate = new Date(date),
+        month = '' + (newDate.getMonth() + 1),
+        day = '' + newDate.getDate(),
+        year = newDate.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
     render(){
 
         const { data } = this.props;
 
         const gridImages = data.map( (data) => (
+
                 <div className="gallery__item" key={data.photo_id}>
                     <article className="gallery-image flex flex--row flex--wrap">
                         <a href={data.photo_src}>
                             <img className="gallery-image__img lazy" src={data.photo_src} alt={data.photo_desc} />
                         </a>
-                        <h2 className="gallery-image__title">{data.photo_title} - <span className="gallery-image__album">Some Stuff</span></h2>
+                        <h2 className="gallery-image__title">{data.photo_title} - <span className="gallery-image__album">{this.formatDate(data.photo_date_added)}</span></h2>
                     </article>
                 </div>
         ));
