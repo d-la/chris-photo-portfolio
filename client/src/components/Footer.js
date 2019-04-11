fc5b1f8e24081bb40106b5e08b1f9d0c13c7fbfd
Daniel La <la.danieln@gmail.com>
2019-04-11 14:49:11 -0400

    Footer component added along with social media links

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer className="footer flex flex--row">
+            <p className="footer__copy">
+                &copy; Christopher Dinh 2019
+            </p>
+            <p className="footer__social">
+                <a href="https://www.instagram.com/cd.nyc/" target="_blank" rel="noopener noreferrer" className="footer__link">
+                    <i className="fa fa-instagram"></i>
+                </a>
+                <a href="https://www.youtube.com/channel/UCjlJ0X226cjB-HT4M7SAj1g" target="_blank" rel="noopener noreferrer" className="footer__link">
+                    <i className="fa fa-youtube"></i>
+                </a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
