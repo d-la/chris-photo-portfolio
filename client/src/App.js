6cb70c9b00cc923009c97b56b4cb150b72c6ba58
Daniel La <la.danieln@gmail.com>
2019-04-11 14:00:38 -0400

    Static category name props passed to Gallery components

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,9 @@ class App extends Component {
 				<Navbar />
 				<Route exact path="/" component={Welcome} />
 				<Route path="/contact" component={Contact} />
-				<Route path="/people-ive-met" render={(props) => <Gallery categoryId={1} />} />
-				<Route path="/the-world-around-me" render={(props) => <Gallery categoryId={2} />} />
-				<Route path="/on-the-road" render={(props) => <Gallery categoryId={3} />} />
+				<Route path="/people-ive-met" render={(props) => <Gallery categoryId={1} categoryName="People I've Met" />} />
+				<Route path="/the-world-around-me" render={(props) => <Gallery categoryId={2} categoryName="The World Around Me" />} />
+				<Route path="/on-the-road" render={(props) => <Gallery categoryId={3} categoryName="On the Road" />} />
 				<Route path="/login" component={Login} />
 			</Router>
 		);
