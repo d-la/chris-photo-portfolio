9464704a8a1df7a0cd1a1a9f4c4c6a4fc41e114a
Daniel La <la.danieln@gmail.com>
2019-04-11 14:50:11 -0400

    Import footer component and add it to the view

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import ContactInfo from './ContactInfo';
 import ContactForm from './ContactForm';
+import Footer from './Footer';
 
 class Contact extends Component{
 
     render(){
         return(
-            <main className="main">
-                <section className="contact">
-                    <div className="flex flex--row">
-                        <ContactInfo />
-                        <ContactForm />
-                    </div>
-                </section>
-            </main>
+            <div>
+                <main className="main">
+                    <section className="contact">
+                        <div className="flex flex--row">
+                            <ContactInfo />
+                            <ContactForm />
+                        </div>
+                    </section>
+                </main>
+                <Footer />
+            </div>
         )
     }
 }
