a187ca0478c7319a82dc9bae425d5cad78276b4e
Daniel La <la.danieln@gmail.com>
2019-04-11 22:51:30 -0400

    changes to html being rendered. updated selectSubCategory

--- client/src/components/SubCategories.js
+++ client/src/components/SubCategories.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 
 class SubCategories extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.selectSubCategory = this.selectSubCategory.bind(this);
+    }
+
+    selectSubCategory(e){
+
+        const { selectSpecificSubCategory } = this.props;
+
+        let subCategoryId = e.currentTarget.dataset.subcategory;
+
+        selectSpecificSubCategory(subCategoryId);
+    }
+
     render(){
 
-        const { subCategoryTitles } = this.props;
+        const { subCategoryList } = this.props;
 
-        let subCategoryHtml = subCategoryTitles.map( (subCategoryName, index) => (
-            <li className="albums__item" key={index}>
-                <a href="/albums/" className="albums__link">
-                    {subCategoryName}
-                    <span className="albums__slider"></span>
-                </a>
+        let subCategoryHtml = subCategoryList.map( (data) => (
+            <li className="albums__item" key={data.subcategory_id} data-subcategory={data.subcategory_id} onClick={this.selectSubCategory}>
+                {data.subcategory_title}
+                <span className="albums__slider"></span>
             </li>
         ));
 
