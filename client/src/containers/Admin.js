1f0cfe61de2a749c85cd256ca2a389f0849295d7
Daniel La <la.danieln@gmail.com>
2019-04-18 14:59:24 -0400

    Added new container directory and Admin container used to render admin pages

---
+++ client/src/containers/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Admin extends Component {
+
+    render(){
+        return(
+            <main className="admin">
+                <h1 className="page-title"></h1>
+            </main>
+        )
+    }
+}
+
+export default Admin;
