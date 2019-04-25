396b512cd424224471b4c85021159560ae906420
Daniel La <la.danieln@gmail.com>
2019-04-25 01:07:23 -0400

    Use props to display image in modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 class Modal extends Component{
 
     render(){
+
+        const { galleryImageSrc, galleryImageAlt } = this.props;
+
         return(
             <div className="modal">
+                <img src={galleryImageSrc} alt={galleryImageAlt} height="100%" width="100%" />
             </div>
         )
     }
