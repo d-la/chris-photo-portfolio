8d96cb92b75b541fd7ad08c8f21cd75881e125fb
Daniel La <la.danieln@gmail.com>
2019-04-25 00:16:28 -0400

    Import the Modal component

--- client/src/components/ModalContainer.js
+++ client/src/components/ModalContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Modal from './Modal';
 
 class ModalContainer extends Component{
 
     render(){
         return(
             <div className="modal-container">
+                <Modal />
             </div>
         )
     }
