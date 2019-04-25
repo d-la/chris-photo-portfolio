aa3f3e0d01bca7efbffac347bd40e40f9d2891a3
Daniel La <la.danieln@gmail.com>
2019-04-25 00:31:50 -0400

    Added state and toggleModal method to GalleryGrid. Pass these down to ModalContainer component

--- client/src/components/GalleryGrid.js
+++ client/src/components/GalleryGrid.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import ModalContainer from './ModalContainer';
 
 class GalleryGrid extends Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
 
         this.formatDate = this.formatDate.bind(this);
     }
@@ -27,17 +32,29 @@ class GalleryGrid extends Component{
         return [year, month, day].join('-');
     }
 
+    toggleModal = () => {
+        const { isModalOpen } = this.state;
+
+        if (isModalOpen){
+            this.setState({isModalOpen: false});
+        } else {
+            this.setState({isModalOpen: true});
+        }
+
+    }
+
     render(){
 
         const { data } = this.props;
+        const { isModalOpen } = this.state;
 
         const gridImages = data.map( (data) => (
 
                 <div className="gallery__item" key={data.photo_id}>
                     <article className="gallery-image flex flex--row flex--wrap">
-                        <a href={data.photo_src}>
-                            <img className="gallery-image__img lazy" src={data.photo_src} alt={data.photo_desc} />
-                        </a>
+                        {/* <a href={data.photo_src}> */}
+                            <img className="gallery-image__img lazy" src={data.photo_src} alt={data.photo_desc} onClick={this.toggleModal} />
+                        {/* </a> */}
                         <h2 className="gallery-image__title">{data.photo_title} - <span className="gallery-image__album">{this.formatDate(data.photo_date_added)}</span></h2>
                     </article>
                 </div>
@@ -45,9 +62,13 @@ class GalleryGrid extends Component{
 
 
         return(
-            <section className="gallery grid grid--gallery grid--gap-1">
-                { gridImages }
-            </section>
+
+            <Fragment>
+                <section className="gallery grid grid--gallery grid--gap-1">
+                    { gridImages }
+                </section>
+                <ModalContainer isModalOpen={isModalOpen} toggleModal={this.toggleModal} />
+            </Fragment>
         )
     }
 }
