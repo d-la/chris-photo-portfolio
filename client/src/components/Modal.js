1b2d91fa0716119eac5f451fb349979328fc77ee
Daniel La <la.danieln@gmail.com>
2019-04-24 23:58:30 -0400

    Base component code for Modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ModalContainer extends Component{
+
+    render(){
+        return(
+            <div className="modal">
+            </div>
+        )
+    }
+}
+
+export default ModalContainer;
