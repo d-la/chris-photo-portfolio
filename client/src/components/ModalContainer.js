137cb562fa0e77f83f927533e156eb75bb484c33
Daniel La <la.danieln@gmail.com>
2019-04-26 01:36:58 -0400

    Change closeModal so we can pass the method to Modal component. Pass other props down to Modal component

--- client/src/components/ModalContainer.js
+++ client/src/components/ModalContainer.js
@@ -5,19 +5,19 @@ class ModalContainer extends Component{
 
     closeModal = (e) => {
         const { toggleModal } = this.props;
-        if (e.target.className === 'modal-container'){
+        if ((e.target.className === 'modal-container') || (e.target.className === 'modal__close')){
             toggleModal();
         }
     }
 
     render(){
 
-        const { isModalOpen, galleryImageSrc, galleryImageAlt } = this.props;
+        const { isModalOpen, galleryImageSrc, selectedGalleryImageIndex, galleryImageAlt, dataLength, setNewModalData } = this.props;
 
         if ( isModalOpen ) {
             return(
                 <div className="modal-container" onClick={this.closeModal}>
-                    <Modal galleryImageSrc={galleryImageSrc} galleryImageAlt={galleryImageAlt} />
+                    <Modal galleryImageSrc={galleryImageSrc} galleryImageAlt={galleryImageAlt} selectedGalleryImageIndex={selectedGalleryImageIndex} dataLength={dataLength} setNewModalData={setNewModalData} closeModal={this.closeModal} />
                 </div>
             )
         } else { 
