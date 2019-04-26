1a97d8e12a8597ac7b329a335016013604d57095
Daniel La <la.danieln@gmail.com>
2019-04-25 20:19:06 -0400

    Added HTML for modal actions

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,7 +8,19 @@ class Modal extends Component{
 
         return(
             <div className="modal">
-                <img src={galleryImageSrc} alt={galleryImageAlt} height="100%" width="100%" />
+                <div className="modal__close">
+                    &times;
+                </div>
+
+                <div className="modal__prev">
+                    <i className="fa fa-chevron-left"></i>
+                </div>
+
+                <img src={galleryImageSrc} alt={galleryImageAlt} className="modal__img" />
+
+                <div className="modal__next">
+                    <i className="fa fa-chevron-right"></i>
+                </div>
             </div>
         )
     }
