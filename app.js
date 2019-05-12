be83c33eab3bd4685594929bdd1eab304f6fe21a
Daniel La <la.danieln@gmail.com>
2019-05-12 15:49:51 -0400

    Restructure files to allow for heroku deployment

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let express = require('express'),
     subcategoryRoutes = require('./routes/subcategory');
     photoRoutes = require('./routes/photo'),
     contactRoutes = require('./routes/contact'),
-    cors = require('cors');
+    cors = require('cors'),
+    path = require('path');
 
 const errorHandler = require('./handlers/error');
 
@@ -20,15 +21,23 @@ app.use('/api/subcategory', cors(), subcategoryRoutes);
 app.use('/api/photo', cors(), photoRoutes);
 app.use('/api/contact', cors(), contactRoutes);
 
+// Serve static files from react app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use( (req, res, next) => {
     let error = new Error('Not Found');
     error.status = 404;
     next(error);
 });
 
+// Catch all for requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.use(errorHandler);
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
